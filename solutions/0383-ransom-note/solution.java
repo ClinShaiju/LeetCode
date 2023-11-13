0ecf8aa18a8d8d6cbbc07d166ca3284d75075c5c
ClinShaiju <63211630+ClinShaiju@users.noreply.github.com>
2023-11-13 00:35:36 +0000

    [LeetCode Sync] Runtime - 2 ms (79.92%), Memory - 41.7 MB (100.00%)

--- solutions/0383-ransom-note/solution.java
+++ solutions/0383-ransom-note/solution.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<String, Integer> mag = new HashMap();
-        for (int i = 0; i < magazine.length(); i++) {
-            String currentLetter = magazine.substring(i, i+1);
-            if (mag.containsKey(currentLetter)) mag.put(currentLetter, mag.get(currentLetter)+1);
-            else mag.put(currentLetter, 1);
-        }
-        while (ransomNote.length() != 0) {
-            String currentLetter = ransomNote.substring(0, 1);
-            if (mag.containsKey(currentLetter)) {
-                mag.put(currentLetter, mag.get(currentLetter) - 1);
-                if (mag.get(currentLetter) == 0) mag.remove(currentLetter);
-                ransomNote = ransomNote.substring(1);
+              int map[] = new int[128];
+        for (char c: ransomNote.toCharArray()){
+            int ind = magazine.indexOf(c, map[c]);
+            if (ind == - 1) 
+            {
+            System.gc();
+            return false;
             }
-            else return false;
+            else
+                map[c] = ind + 1;
+
         }
+        System.gc();
         return true;
     }
 }
