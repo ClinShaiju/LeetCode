7740c88682c37226ea16cb20a56ad25052d2ee8e
ClinShaiju <63211630+ClinShaiju@users.noreply.github.com>
2024-04-14 20:50:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (47.06%)

---
+++ solutions/0404-sum-of-left-leaves/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sumOfLeftLeaves(root, false);
+    }
+
+    private int sumOfLeftLeaves(TreeNode root, boolean left) {
+        if (root != null) {
+            if (left && root.left == null && root.right == null) return root.val;
+            return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right, false);
+        }
+        return 0;
+    }
+}
