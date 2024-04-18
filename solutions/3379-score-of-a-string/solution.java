399ae08f529a29d04719b825f8cf7227c7496c98
ClinShaiju <63211630+ClinShaiju@users.noreply.github.com>
2024-04-18 02:02:56 +0000

    [LeetCode Sync] Runtime - 12 ms (99.85%), Memory - 42.6 MB (5.51%)

--- solutions/3379-score-of-a-string/solution.java
+++ solutions/3379-score-of-a-string/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
         int sum = 0;
-        for (int i = 1; i < s.length(); i++) {
-            sum += Math.abs(s.charAt(i)-s.charAt(i-1));
-        }
+        for (int i = 1; i < s.length(); i++) sum += Math.abs(s.charAt(i)-s.charAt(i-1));
+        System.gc();
         return sum;
     }
 }
