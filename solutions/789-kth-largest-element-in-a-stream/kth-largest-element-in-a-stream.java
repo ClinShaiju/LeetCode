a2932c422073b6811d2cd2793b3fd145d50eb7d8
ClinShaiju <63211630+ClinShaiju@users.noreply.github.com>
2024-08-15 21:05:48 -0500

    Time: 400 ms (8.54%) | Memory: 47.1 MB (30.56%) - LeetSync

---
+++ solutions/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -0,0 +1,25 @@
+class KthLargest {
+    private final int k;
+    private final int[] nums;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.nums = new int[k];
+
+        Arrays.sort(nums);
+        Arrays.fill(this.nums, Integer.MIN_VALUE);
+        for (int i = 0; i < k && i < nums.length; i++) this.nums[this.nums.length - 1 - i] = nums[nums.length - 1 - i];
+    }
+
+    public int add(int val) {
+        if (val > nums[0]) nums[0] = val;
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
